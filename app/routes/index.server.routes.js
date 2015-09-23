9b50237257cbddd3e6d7c3f7643dbbd7d63a639b
Ivan834 <pavlov.ivan0415@outlook.com>
2015-09-23 15:01:06 +0400

    update

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,4 +1,6 @@
+// server controller route
 module.exports = function(app){
 	var index = require('../controllers/index.server.controller');
-	app.get('/', index.render);
+	//app.get('/', index.render);
+	app.get('/',index.render);
 }
