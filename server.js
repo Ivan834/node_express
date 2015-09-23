5d74783fd87750daf3ec98dec95833fffc7f75f2
Ivan834 <pavlov.ivan0415@outlook.com>
2015-09-23 14:55:32 +0400

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var mongoose = require('./config/mongoose');
+var	express = require('./config/express');
+
+var db = mongoose();
+var app = express();
+app.listen(5000);
+
+module.exports = app;
+
+console.log('Server running at port 5000 ...');
